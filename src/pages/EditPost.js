cd81dee786f9c29de6847f6ee148abd42d5f5acb
Luciano <lucianobfsantana@hotmail.com>
2021-12-15 19:20:34 -0300

    testes

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -1,4 +1,3 @@
-import { Link, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import api from "../apis/api";
